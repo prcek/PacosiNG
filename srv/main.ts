5b1711c3224cc6ccc9d76d77e6e3db19b5a5dc66
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-01-16 15:06:45 +0100

    serve dist static

--- srv/main.ts
+++ srv/main.ts
@@ -2,10 +2,13 @@ import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
+import * as responseTime from 'response-time';
+import {join} from 'path';
 
 
 import {createAndRegisterApolloServer} from './apollo';
 
+const DIST_FOLDER = join(process.cwd(), 'dist/pacosi');
 
 
 (async () => {
@@ -13,7 +16,13 @@ import {createAndRegisterApolloServer} from './apollo';
     const app = express();
     const PORT = process.env.PORT || 4000;
 
+    app.set('view engine', 'html');
+    app.set('views', DIST_FOLDER);
+    app.use(responseTime());
     app.use(cookieParser());
+    app.get('*.*', express.static(DIST_FOLDER, {
+      maxAge: '1y'
+    }));
 
     await createAndRegisterApolloServer(app);
 
