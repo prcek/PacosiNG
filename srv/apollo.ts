3b5d3134513d06404d886ca51c1b51cfa3f9a684
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-01-16 10:57:37 +0100

    create and register fce

---
+++ srv/apollo.ts
@@ -0,0 +1,31 @@
+
+import {Express} from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { createDataSources } from './datasources';
+import { schema } from './schema';
+import { createStore } from './store';
+
+let global_counter = 1;
+
+export async function createAndRegisterApolloServer(app:Express) {
+
+    const store = await createStore();
+
+    const apollo_server = new ApolloServer({
+        schema,
+        dataSources: () => (createDataSources(store)),
+        context: ({ req }) => {
+            // get the user token from the headers
+            console.log("setting context ");
+            const val = 'pepa' + global_counter;
+            global_counter++;
+            return { user: { name: val} };
+        },
+        introspection: true,
+        playground: true
+    });
+    
+   
+    apollo_server.applyMiddleware({app,path:'/graphql'});
+
+}
--- srv/main.ts
+++ srv/main.ts
@@ -3,38 +3,20 @@ import 'reflect-metadata';
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
 
-import { ApolloServer } from 'apollo-server-express';
-import { createDataSources } from './datasources';
-import { schema } from './schema';
-import { createStore } from './store';
 
-let global_counter = 1;
+import {createAndRegisterApolloServer} from './apollo';
+
 
 
 (async () => {
     
     const app = express();
     const PORT = process.env.PORT || 4000;
-
-    const store = await createStore();
-
-    const apollo_server = new ApolloServer({
-        schema,
-        dataSources: () => (createDataSources(store)),
-        context: ({ req }) => {
-            // get the user token from the headers
-            console.log("setting context ");
-            const val = 'pepa' + global_counter;
-            global_counter++;
-            return { user: { name: val} };
-        },
-        introspection: true,
-        playground: true
-    });
     
     app.use(cookieParser());
-    apollo_server.applyMiddleware({app,path:'/graphql'});
-    
+
+    await createAndRegisterApolloServer(app);
+
     app.listen(PORT, () => {
       console.log(`Node Express server listening on http://localhost:${PORT}`);
     });
