4b12b3e3c1033b592263d0f7d3f843848898f267
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-01-18 10:36:12 +0100

    injector test

--- src/app/graphql.server.module.ts
+++ src/app/graphql.server.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from '@angular/core';
+import {NgModule, Injector} from '@angular/core';
 import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
 import {HttpLinkModule, HttpLink} from 'apollo-angular-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
 const uri = 'http://localhost:4000/graphql'; // <-- add the URL of the GraphQL server here
-export function createApolloServer(httpLink: HttpLink) {
-  console.log('SERVER createApollo Options');
+export function createApolloServer(httpLink: HttpLink, injector: Injector) {
+  console.log('SERVER createApollo Options', injector);
+  // injector.get()
   return {
     link: httpLink.create({uri}),
     cache: new InMemoryCache(),
@@ -18,7 +19,7 @@ export function createApolloServer(httpLink: HttpLink) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApolloServer,
-      deps: [HttpLink],
+      deps: [HttpLink, Injector],
     },
   ],
 })
