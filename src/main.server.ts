2ad7ceb86ed2a9d22e2b556ce6c5949f4c991ccd
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-01-28 14:29:35 +0100

    up

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,4 +1,5 @@
 
 export { AppServerModule } from './app/app.server.module';
 export { TEST_TOKEN } from './app/token';
-export { AUTH_USER_INFO_TOKEN } from './app/auth.service';
+export { AUTH_USER_INFO_TOKEN, IUserInfo} from './app/auth.service';
+
