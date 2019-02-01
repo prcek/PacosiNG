c4024c62f97553257da058a6cfce9595ad8ef8a7
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-02-01 12:11:39 +0100

    celendar editor

--- src/app/calendar-editor/calendar-editor.component.ts
+++ src/app/calendar-editor/calendar-editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
-import { ICalendar } from '../calendar.service';
+import { ICalendar, CalendarService } from '../calendar.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -14,22 +14,38 @@ export class CalendarEditorComponent implements OnInit {
 
   calendarForm = new FormGroup({
     name: new FormControl('', { validators: Validators.required}),
+    span: new FormControl(15, { validators: [Validators.required, Validators.min(5), Validators.max(60)]}),
   });
   error_msg: string;
   submitted = false;
 
 
-  constructor() { }
+  constructor(private calendarService: CalendarService) { }
 
   ngOnInit() {
     console.log('CalendarEditorComponent.ngOnInit', this.calendar);
     this.calendarForm.setValue({
       name: this.calendar.name,
+      span: this.calendar.span
     });
   }
 
   onSubmit() {
-    alert('todo');
+     // TODO: Use EventEmitter with form value
+     const uc: ICalendar = {id: this.calendar.id, ...this.calendarForm.getRawValue()};
+     this.submitted = true;
+     this.calendarForm.disable();
+     this.error_msg = null;
+     console.log('CalendarEditorComponent.onSubmit', uc);
+     this.calendarService.updateCalendar(uc).subscribe((r) => {
+       this.updated.emit(r);
+       this.submitted = false;
+       this.calendarForm.enable();
+     }, (err) => {
+       this.submitted = false;
+       this.calendarForm.enable();
+       this.error_msg = err;
+     });
   }
 
 }
