f2fb28c436942e5d7e2a82c0f81b25736f08db9f
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-02-07 16:49:43 +0100

    interval validation

--- src/app/calendar-editor/calendar-oht-editor.component.ts
+++ src/app/calendar-editor/calendar-oht-editor.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ICalendar } from '../calendar.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors } from '@angular/forms';
+
+
+export const timeIntervalValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+  const time = control.get('time');
+  const time_end = control.get('time_end');
+  return time && time_end && time.valid && time_end.valid && time_end.value <= time.value ? {'invalidInterval': true} : null;
+};
 
 @Component({
   selector: 'app-calendar-oht-editor',
@@ -15,7 +22,7 @@ export class CalendarOhtEditorComponent implements OnInit {
     name: new FormControl('', { validators: Validators.required}),
     time: new FormControl(null, { validators: Validators.required}),
     time_end: new FormControl(null, { validators: Validators.required}),
-  });
+  }, { validators: timeIntervalValidator });
 
   constructor() { }
 
