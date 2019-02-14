59c9235b48976b6537560ae378bf69a19b135052
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-02-14 10:15:55 +0100

    confirm et del dialog

--- src/app/calendar-page/calendar-et-page.component.ts
+++ src/app/calendar-page/calendar-et-page.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { CalendarService, ICalendar, ICalendarEventType } from '../calendar.service';
+import { MatDialog } from '@angular/material';
+import { DialogConfirmComponent } from '../dialogs/dialog-confirm.component';
 
 @Component({
   selector: 'app-calendar-et-page',
@@ -12,7 +14,11 @@ export class CalendarEtPageComponent implements OnInit {
   calendar: ICalendar;
   event_types: ICalendarEventType[];
   displayedColumns: string[] = ['name', 'color', 'len', 'order', 'actions'];
-  constructor(private route: ActivatedRoute, private location: Location, private calendarService: CalendarService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private calendarService: CalendarService,
+    public dialog: MatDialog) { }
 
   ngOnInit() {
     this.getCalendarWithETs();
@@ -24,6 +30,16 @@ export class CalendarEtPageComponent implements OnInit {
   }
   goBack(): void {
     this.location.back();
+  }
+  onDelete(event_type) {
+      const dialogRef = this.dialog.open(DialogConfirmComponent, {
+        width: '450px',
+        data: {title: 'Smazání typu události', content: 'opravdu smazat typ události "' + event_type.name + '" ?!'}
+      });
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed', result);
+        this.getCalendarWithETs();
+      });
   }
   get diag() {
     return JSON.stringify({calendar: this.calendar, event_types: this.event_types});
