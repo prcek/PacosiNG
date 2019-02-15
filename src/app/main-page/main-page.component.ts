40a2316a5c89e8402c7bc3dec4bdd45a1be0b0d8
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-02-15 10:29:18 +0100

    cals dashboard test

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarService } from '../calendar.service';
+import { CalendarService, ICalendarStatus } from '../calendar.service';
 import * as M from 'moment';
 
 
@@ -9,16 +9,22 @@ import * as M from 'moment';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  test: any;
+  cals: ICalendarStatus[];
   loading = true;
   constructor(private calendarService: CalendarService) { }
 
   ngOnInit() {
+    this.getCalendars();
+  }
+  getCalendars() {
     const now = M().startOf('day');
-
+    this.loading = true;
     this.calendarService.getCalendarsStatus(now.toDate(), M(now).add(10, 'days').toDate())
-      .subscribe((r) => { this.test = r; this.loading = false; } );
+      .subscribe((r) => {
+          this.cals = r;
+          this.loading = false;
+      } );
   }
 
-
+  get diag() { return JSON.stringify({cals: this.cals}); }
 }
