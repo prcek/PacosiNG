faffab4c2796810e7bb9c57cb980df3b51271e10
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-02-18 17:28:30 +0100

    up

--- src/app/calendar-editor/calendar-event-editor.component.ts
+++ src/app/calendar-editor/calendar-event-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { ICalendarEvent, ICalendar, ICalendarEventType } from '../calendar.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calendar-event-editor',
@@ -12,9 +13,32 @@ export class CalendarEventEditorComponent implements OnInit {
   @Input() event: ICalendarEvent;
   @Input() event_types: ICalendarEventType;
   @Input() new_mode: boolean;
+
+
+  eventForm = new FormGroup({
+    name: new FormControl('', { validators: Validators.required}),
+    color: new FormControl('', { validators: Validators.required}),
+    begin: new FormControl(0, { validators: Validators.required}),
+    len: new FormControl(0, { validators: Validators.required}),
+  });
+  error_msg: string;
+  submitted = false;
+
+
   constructor() { }
 
   ngOnInit() {
+    console.log('CalendarEventEditorComponent.ngOnInit', this.event);
+    if (this.new_mode) {
+
+    } else {
+      this.eventForm.setValue({
+        name: this.event.name,
+        color: this.event.color,
+        begin: this.event.begin,
+        len: this.event.len
+      });
+    }
   }
 
 }
