9a8781bb0f8dac098ac299ba41af645cb7ff0ef7
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-02-21 18:33:03 +0100

    up

--- src/app/user-page/user-new-page.component.ts
+++ src/app/user-page/user-new-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { CalendarService, ICalendar } from '../calendar.service';
 @Component({
   selector: 'app-user-new-page',
   templateUrl: './user-new-page.component.html',
   styleUrls: ['./user-new-page.component.css']
 })
 export class UserNewPageComponent implements OnInit {
-
-  constructor(private location: Location) { }
+  calendars: ICalendar[];
+  constructor(private location: Location, private calendarService: CalendarService) { }
 
   ngOnInit() {
+    this.calendarService.getCalendars().subscribe(cals => this.calendars = cals);
   }
 
   goBack(): void {
