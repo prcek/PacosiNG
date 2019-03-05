c82c3ddd79b6f18fe07560b051072e2e2e84baaf
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-03-05 13:12:42 +0100

    locations table

--- src/app/locations-page/locations-page.component.ts
+++ src/app/locations-page/locations-page.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { tap, map, switchMap } from 'rxjs/operators';
+import { MatSlideToggleChange } from '@angular/material';
+import { ILocation, LocationService } from '../location.service';
 
 @Component({
   selector: 'app-locations-page',
   templateUrl: './locations-page.component.html',
   styleUrls: ['./locations-page.component.css']
 })
-export class LocationsPageComponent implements OnInit {
+export class LocationsPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  opt$: Subscription;
+  all: boolean;
+  locations: ILocation[];
+  constructor(private router: Router, private route: ActivatedRoute, private locationService: LocationService) { }
 
   ngOnInit() {
+    console.log('LocationsPageComponent.ngOnInit');
+    this.opt$ = this.route.paramMap.pipe(
+      map(params => (params.get('all') === 'yes')),
+      tap(all => this.all = all),
+      switchMap((all: boolean) => this.locationService.getLocations(all))
+    ).subscribe(locations => this.locations = locations);
   }
+  ngOnDestroy(): void {
+    this.opt$.unsubscribe();
+  }
+  onAll(event: MatSlideToggleChange) {
+    this.router.navigate([{ all: event.checked ? 'yes' : 'no' }]);
+  }
+  get displayedColumns(): string[] {
+    if (this.all) {
+      return ['archived', 'name', 'address', 'actions'];
+    } else {
+      return ['name', 'address', 'actions'];
+    }
+  }
+
+  onEdit(loc: ILocation) {
+    this.router.navigate(['/locations/edit', loc._id]);
+  }
+
+
 
 }
