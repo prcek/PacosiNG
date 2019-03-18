681f665e55f1730a36575e5dff95ae683aca8d3d
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-03-18 16:56:26 +0100

    gitver

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as data from './git-version.json';
 
+const git = data.default;
 if (environment.production) {
   enableProdMode();
 }
-
+console.log('GIT_HASH', git.hash);
 
 document.addEventListener('DOMContentLoaded', () => {
     platformBrowserDynamic().bootstrapModule(AppModule)
