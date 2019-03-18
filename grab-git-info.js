fb650481d8208cb2ed0ac072c9c10904c181f2cb
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-03-18 17:08:12 +0100

    up

--- grab-git-info.js
+++ grab-git-info.js
@@ -1,8 +1,14 @@
-const { gitDescribeSync } = require('git-describe');
+
+
+//const { gitDescribeSync } = require('git-describe');
 const { writeFileSync } = require('fs');
 const path = require('path');
-const info = gitDescribeSync();
-const infoJson = JSON.stringify(info, null, 2);
+//const info = gitDescribeSync();
+//const infoJson = JSON.stringify(info, null, 2);
+
+
+const ver = process.env.SOURCE_VERSION || '<undefined>'
+const infoJson = JSON.stringify({hash: ver});
 writeFileSync(path.join(__dirname, 'srv/git-version.json'), infoJson);
 writeFileSync(path.join(__dirname, 'src/git-version.json'), infoJson);
 
