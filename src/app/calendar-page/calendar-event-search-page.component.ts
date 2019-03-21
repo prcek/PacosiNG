6467b28c7b7e07a0bf91ab44e2877e6948df3ff7
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-03-21 10:01:31 +0100

    search wip

--- src/app/calendar-page/calendar-event-search-page.component.ts
+++ src/app/calendar-page/calendar-event-search-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { CalendarService } from '../calendar.service';
+import { CalendarService, ICalendarEvent } from '../calendar.service';
 import * as R from 'ramda';
 import * as M from 'moment';
 @Component({
@@ -15,8 +15,10 @@ export class CalendarEventSearchPageComponent implements OnInit {
   submitted = false;
   wait = false;
   cal_ids = null;
+  events: ICalendarEvent[];
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private location: Location,
     private calendarService: CalendarService
   ) { }
@@ -34,23 +36,35 @@ export class CalendarEventSearchPageComponent implements OnInit {
         this.cal_ids = ids;
       }
     }
+    if (this.search_string && this.search_string.trim().length) {
+      this.doSearch();
+    }
   }
   goBack(): void {
     this.location.back();
   }
+
+  get displayedColumns(): string[] {
+      return [ /*'id',*/ 'calendar_id', 'day', 'time', 'event_name', 'first_name', 'last_name', 'year', 'phone', 'actions'];
+  }
+
+
   doSearch() {
     this.wait = true;
     const sday = M().utc().startOf('isoWeek').subtract(30, 'days').toDate();
-    const eday = M().utc().startOf('isoWeek').add(30, 'days').toDate();
+    const eday = M().utc().startOf('isoWeek').add(1, 'year').toDate();
     this.calendarService.searchCalendarEvents(this.search_string, this.cal_ids, sday, eday).subscribe(evs => {
-      console.log('evs', evs);
+      // console.log('evs', evs);
+      this.events = evs;
       this.wait = false;
     });
   }
 
-  onSearch(event) {
-    alert('onSearch' +  JSON.stringify(event));
-    this.search_string = event;
+  onSearch(str) {
+    this.search_string = str;
     this.doSearch();
+  }
+  onView(event) {
+    this.router.navigate(['calendars', 'events', event.calendar_id, 'day', M.utc(event.day).format('YYYY-MM-DD')]);
   }
 }
