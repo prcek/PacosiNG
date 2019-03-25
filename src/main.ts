fc5a3d732f437b088465c158b33dcbb1a4bb396a
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-03-25 14:55:12 +0100

    logrocket

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as LogRocket from 'logrocket';
+
 if (environment.production) {
   enableProdMode();
+  LogRocket.init('evmn92/den-doktora');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
