5eb0d55b28cfd54a932a3d94cee5a156f919762f
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-04-03 17:39:57 +0200

    fix

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -59,7 +59,7 @@ export class MainPageComponent implements OnInit {
           this.loc_ids =  R.uniq(R.map<ICalendarStatus>(cs => cs.calendar.location_id, r));
           // console.log('CALS', r );
           if (this.pref_loc_id && this.loc_ids.length > 1) {
-            const rf = R.filter( cs => cs.calendar.location_id === this.pref_loc_id, r);
+            const rf = R.filter<ICalendarStatus>( cs => cs.calendar.location_id === this.pref_loc_id, r);
             this.grid = this.calendarService.convertStatuses2Grid(rf);
           } else {
             this.grid = this.calendarService.convertStatuses2Grid(r);
