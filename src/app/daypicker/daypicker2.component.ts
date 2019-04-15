275d36fcda8de64f297ad990d1d685e84f5ab8e4
Tomas Hluchan <tomas.hluchan@gmail.com>
2019-04-15 12:22:11 +0200

    design daypicker2

--- src/app/daypicker/daypicker2.component.ts
+++ src/app/daypicker/daypicker2.component.ts
@@ -6,8 +6,39 @@ import * as M from 'moment';
 const FIRST_DAY_CORR = [6, 0, 1, 2, 3, 4, 5];
 
 interface IDaySlot {
-  date: Date | null;
+  blank: boolean;
+  day: number;
 }
+
+interface IMonthPage {
+  year: number;
+  month: number;
+  days: IDaySlot[];
+}
+
+function _calcMonthPage(year: number, month: number): IMonthPage {
+  const first_day = M.utc('' + year + '-' + month + '-01', 'YYYY-MM-DD');
+  // console.log('FIRST_DAY', first_day.toISOString());
+
+  const first_day_off = FIRST_DAY_CORR[first_day.isoWeekday()];
+  const month_days = M(first_day).add(1, 'month').diff(first_day, 'days');
+
+  const dr = (first_day_off + month_days) % 7;
+  const last_day_addon = dr ? 7 - dr : 0;
+
+ // const total_slots = first_day_off + month_days + last_day_addon;
+
+  const xa = R.map<number, IDaySlot>( i => ({ blank: true, day: -1}), R.range(0, first_day_off));
+  const xb = R.map<number, IDaySlot>( i => ({ blank: false, day: i}), R.range(1, month_days + 1));
+  const xc = R.map<number, IDaySlot>( i => ({ blank: true, day: -1}), R.range(0, last_day_addon));
+
+  return {
+    year,
+    month,
+    days: [...xa, ...xb, ...xc]
+  };
+}
+
 
 
 @Component({
@@ -17,36 +48,24 @@ interface IDaySlot {
 })
 export class Daypicker2Component implements OnInit {
   dayNames = ['Po', 'Út', 'St', 'Čt', 'Pá', 'So', 'Ne'];
+  monthNames = ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'];
 
   @Input() _start_month: string; // = '2019-06-01';
 
-  slots: number[] = [];
-  _first_day_off = 0;
-  _month_days = 0;
-  _last_day_addon = 0;
-  _total_slots = 0;
+
+  monthPage: IMonthPage;
+
   constructor() { }
+
+
 
   ngOnInit() {
     console.log('Daypicker2Component.ngOnInit', this._start_month);
+
+
     const sd = M.utc(this._start_month).startOf('month');
-    this._first_day_off = FIRST_DAY_CORR[sd.isoWeekday()];
 
-    this._month_days = M(sd).add(1, 'month').diff(sd, 'days');
-    const dr = (this._first_day_off + this._month_days) % 7;
-    if (dr) {
-      this._last_day_addon = 7 - dr;
-    } else {
-      this._last_day_addon = 0;
-    }
-    this._total_slots = this._first_day_off + this._month_days + this._last_day_addon;
-
-
-    const xa =  R.map( i => null, R.range(0, this._first_day_off));
-    const xb =  R.map( i => i, R.range(1, this._month_days + 1));
-    const xc = R.map( i => null, R.range(0, this._last_day_addon));
-    this.slots =  [...xa, ...xb, ...xc];
-    // console.log("XXBLA",xa,xb,xc);
+    this.monthPage = _calcMonthPage(sd.year(), sd.month() + 1);
     }
 
 }
